6553cfaf88bb796bea5fa93a53e68fecf70277ee
Wesley Kleim <wesleykleim@gmail.com>
2023-09-18 20:49:17 -0300

    Delete e Update

--- src/actions/cadaastro.js
+++ src/actions/cadaastro.js
@@ -22,6 +22,66 @@ export async function creat(formData){
 
         
     }
-    revalidatePath("/contas")
-    return{ok: "Conta cadastra com sucesso"}
+    revalidatePath("/cadastro")
+    return{ok: "Cadastro realizado com sucesso"}
+}
+export async function getContas(){
+    await new Promise(r => setTimeout(r, 5000));
+    const response = await fetch(url,  { next: { revalidate: 3600 } })
+    return response.json()
+}
+
+export async function destroy(id){
+    const deleteUrl = url + "/" + id
+
+    const options = {
+        method: "DELETE"
+    }
+
+    const response = await fetch(deleteUrl, options)
+
+    if (!response.ok){
+        const json = await response.json()
+        return {error: "Falha ao apagar conta. Verifique se deletou o cadastro corretamente "}
+    }
+
+    revalidatePath("/cadastro")
+
+
+}
+
+export async function getConta(id){
+    const getUrl = url + "/" + id
+    const response = await fetch(getUrl)
+
+    const json = await response.json()
+
+    if (!response.ok){
+        return {error: "Falha ao carregar usuário cadastrado. " + json.message}
+    }
+
+    return json
+}
+
+export async function update(cadastro){
+    const updateUrl = url + "/" + conta.id
+
+    const options = {
+        method: "PUT",
+        body: JSON.stringify(cadastro) ,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    const response = await fetch(updateUrl, options)
+
+    if (!response.ok){
+        const json = await response.json()
+        return {error: "Erro ao atualizar o cadastro do usuário" + json.message }
+    }
+
+    revalidatePath("/cadastro")
+
+    return {ok: "Cadastro alterada com sucesso"}
 }
--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -4,41 +4,78 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { destroy } from '@/actions/contas';
+import toast from 'react-hot-toast';
+import {useRouter} from 'next/navigation'
 
-export default function DropMenu() {
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
+export default function DropMenu({cadastro}) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  const router = useRouter()
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+  const handleEdit = () => {
+    router.push("/cadastro/"+ cadastro.id +"/edit")
+  }
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const handleDelete = async () => {
+    const response = await destroy(cadastro.id)
+
+    if (response?.error){
+      toast.error(response.error, { style: {
+        borderRadius: '8px',
+        background: '#333',
+        color: '#fff',
+      }})
+    }else{
+      toast.success("Conta apagada com sucesso", { style: {
+        borderRadius: '8px',
+        background: '#333',
+        color: '#fff',
+      }})
+    }
+    setAnchorEl(null);
+  }
 
   return (
     <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-to-line"><path d="M12 17V3"/><path d="m6 11 6 6 6-6"/><path d="M19 21H5"/></svg>
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>apagar</MenuItem>
-        <MenuItem onClick={handleClose}>editar</MenuItem>
-        
-      </Menu>
+      <AlertDialog>
+          <AlertDialogTrigger>
+            <MenuItem>apagar</MenuItem>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Tem certeza que quer apagar essa conta?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Ao apagar a conta, todos os dados serão perdidos. Essa ação não pode ser desfeita.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>cancelar</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDelete}>sim, pode apagar essa conta</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+
+        <MenuItem onClick={handleEdit}>editar</MenuItem>
+      
     </div>
   );
 }
